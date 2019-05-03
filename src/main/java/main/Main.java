e19cdaec117ac783e5f164d2435bdcc836ef4367
Mattl02 <hoemac16@htl-kaindorf.ac.at>
2019-05-03 12:43:53 +0200

    added main class

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import tournamentclass.TournamentClassGUI;
+
+/**
+ *
+ * @author Matthias
+ */
+public class Main {
+    public static void main(String[] args) {
+        TournamentClassGUI tc = new TournamentClassGUI();
+        tc.setVisible(true);
+    }
+}
