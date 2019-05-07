c560e73b3d7618ac8d7bf5e77ca9bef810a8c765
Mattl02 <hoemac16@htl-kaindorf.ac.at>
2019-05-07 09:48:17 +0200

    added documentation in tournamentclasslistmodel

--- src/main/java/tournamentclass/TournamentClassListModel.java
+++ src/main/java/tournamentclass/TournamentClassListModel.java
@@ -12,16 +12,28 @@ public class TournamentClassListModel extends AbstractListModel{
 
     private ArrayList<TournamentClass> classes = new ArrayList<>();
     
+    /**
+     * Adds a class into the list of tournament-classes.
+     * @param c The class to be added to the list of tournament-classes.
+     */
     public void add (TournamentClass c) {
         classes.add(c);
         this.fireIntervalAdded(this, classes.size()-1, classes.size()-1);
     }
     
+    /**
+     * Removes a class from the list of tournament-classes.
+     * @param idx The index of the element to be removed.
+     */
     public void remove(int idx) {
         classes.remove(idx);
         this.fireContentsChanged(this, 0, classes.size()-1);
     }
     
+    /**
+     * Removes multiple elements from the list of tournamet-classes.
+     * @param idx The indices of the elements to be removed.
+     */
     public void remove(int[] idx) {
         for(int i = idx.length-1; i >= 0; i--){
             classes.remove(idx[i]);
@@ -29,11 +41,20 @@ public class TournamentClassListModel extends AbstractListModel{
         this.fireContentsChanged(this, 0, classes.size()-1);
     }
     
+    /**
+     * 
+     * @return The number of tournament-classes in the list.
+     */
     @Override
     public int getSize() {
         return classes.size();
     }
 
+    /**
+     * 
+     * @param i The index of the element to be returned.
+     * @return The object of an element at a specific index.
+     */
     @Override
     public TournamentClass getElementAt(int i) {
         return classes.get(i);
